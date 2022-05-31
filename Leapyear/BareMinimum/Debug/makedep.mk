6abd4e36946089632bc4061d5282cb7bd804be26
Rene Rasmussen <rene069g@elevcampus.dk>
2022-05-31 15:04:30 +0200

    new assignments

--- Leapyear/BareMinimum/Debug/makedep.mk
+++ Leapyear/BareMinimum/Debug/makedep.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-Pointer.cpp
+Pointmath.cpp
 
 src\libraries\Keypad\src\Key.cpp
 
