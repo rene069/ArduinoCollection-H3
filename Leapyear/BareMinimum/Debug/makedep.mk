86cc4027d50e8b4f6838c3412c9ad630d30e16c0
Rene Rasmussen <rene069g@elevcampus.dk>
2022-05-31 13:30:16 +0200

    test

--- Leapyear/BareMinimum/Debug/makedep.mk
+++ Leapyear/BareMinimum/Debug/makedep.mk
@@ -2,7 +2,11 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-RGPtemperatur.cpp
+Pointer.cpp
+
+src\libraries\Keypad\src\Key.cpp
+
+src\libraries\Keypad\src\Keypad.cpp
 
 src\libraries\LiquidCrystal\src\LiquidCrystal.cpp
 
